f8147a7bd35eafa2b1f872cf380d61593e536b7b
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-17 00:16:14 +1300

    Added blog status of draft and publish

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
+  before_action :set_blog, only: %i[ toggle_status show edit update destroy ]
 
   # GET /blogs or /blogs.json
   def index
@@ -50,6 +50,15 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published! 
+    elsif @blog.published?
+      @blog.draft! 
+    end
+    redirect_to blogs_url, notice: 'Post status is updated.'
   end
 
   private
