ea4b045a9005b51824ebba37e917b6088629b4aa
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-22 15:44:15 +1300

    Added custom layouts for blogs and portfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ toggle_status show edit update destroy ]
+  layout "blog"
 
   # GET /blogs or /blogs.json
   def index
