422319610bd619d798b47f0e06c05a52dbe19a8e
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-23 17:15:15 +1300

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ end
 gem "friendly_id", '~> 5.4', '>= 5.4.2'
 gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'bootstrap', '~> 5.1.3'
+gem 'kandit_view_tool', git: 'https://github.com/rwoollett/kandit_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class:"source-greeting")
     end
   end
+
+  def copyright_generator
+    KanditViewTool::Renderer.copyright "Rodney Woollett", "All rights reserved"
+  end
 end
