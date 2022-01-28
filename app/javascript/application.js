47b7744f4f8521f121ebf5eceb11a6618b1c7a72
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-28 20:58:10 +1300

    Installed jquery ui, sortable html5 library and css calls

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery";
+import "bootstrap";
+import Rails from '@rails/ujs';
+import "jquery-ui";
+import "./html_sortable";
 import "@hotwired/turbo-rails";
 import "controllers";
-import "bootstrap";
-
-import Rails from '@rails/ujs';
 
 Rails.start();
+
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
