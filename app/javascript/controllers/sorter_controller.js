7ddcf5f4f355ca3bee9c17bbf68a40ed65fe28f6
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-29 00:49:44 +1300

    Implemented persistentposition updates for drag and drop interface

--- app/javascript/controllers/sorter_controller.js
+++ app/javascript/controllers/sorter_controller.js
@@ -1,7 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
+//var set_positions = undefined
+let set_positions = function() {
+  $('.card').each(function(i) {
+    $(this).attr('data-pos', i + 1);
+  });
+};
+
 export default class extends Controller {
   connect() {
+    set_positions();
     $('.sortable').sortable();
+    $('.sortable').sortable().bind('sortupdate', function(e, ui) {
+      const updated_order = [];
+      set_positions();
+      $('.card').each(function(i) {
+        updated_order.push({
+          id: $(this).data('id'),
+          position: i + 1
+        });
+      });
+      $.ajax({
+        type: 'PUT',
+        url: '/portfolios/sort',
+        data: { order: updated_order
+      }
+      });
+    });
   }
 }
