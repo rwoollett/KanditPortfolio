bd6976cc7af8049cce159898d4022b9354d9ff54
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-31 12:12:06 +1300

    Initial gritter integration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "jquery";
 import "bootstrap";
-// ddimport Rails from '@rails/ujs';
+import Rails from '@rails/ujs';
 import "jquery-ui";
 import "./html_sortable";
 import "cocoon";
 import "@hotwired/turbo-rails";
 import "controllers";
+
+import "./gritterlocal";
+
+Rails.start();
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,10 +6,10 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-#pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
-pin "jquery", to: "jquery3.js", preload: true
-#pin "gritter", to: 'gritter.js', preload: true
-pin "jquery-ui", to: 'jquery-ui.js', preload: true
-pin "cocoon", to: 'cocoon.js', preload: true
-pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
+pin "jquery", to: "jquery3.js"
+pin "jquery-ui", to: 'jquery-ui.js'
+pin "cocoon", to: 'cocoon.js'
+pin "popper", to: 'popper.js'
+pin "bootstrap", to: 'bootstrap.min.js'
+
