e0edd8b133e76380d258a3874baf74d7c6778826
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-30 23:18:25 +1300

    Change logout back to button_to. Removed import of CDN of necessary rails module to make link_to work - it didn't realyy work, button worked best.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "jquery";
 import "bootstrap";
-import Rails from '@rails/ujs';
+// ddimport Rails from '@rails/ujs';
 import "jquery-ui";
 import "./html_sortable";
 import "cocoon";
+
 import "@hotwired/turbo-rails";
 import "controllers";
 
-Rails.start();
+// ddRails.start();
 
 // var ready = undefined;
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
+#pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
 pin "jquery", to: "jquery3.js", preload: true
 pin "jquery-ui", to: 'jquery-ui.js', preload: true
 pin "cocoon", to: 'cocoon.js', preload: true
