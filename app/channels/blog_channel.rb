0b3a37008dddea7a9dd9ca57ae1a0411b022903a
Rodney Woollett <rodney.woollett@gmail.com>
2022-02-02 03:39:18 +1300

    Implemented action cable for comments

--- app/channels/blog_channel.rb
+++ app/channels/blog_channel.rb
@@ -1,9 +1,12 @@
 class BlogChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "blog_#{params['blog_id']}_channel"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def send_comment(data)
+    current_user.comments.create!(content: data['comment'], blog_id: data['blog_id'])
   end
 end
