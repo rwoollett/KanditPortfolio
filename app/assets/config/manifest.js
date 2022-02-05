30d916c2d60751e1b52fc638060e9868bb6101e5
Rodney Woollett <rodney.woollett@gmail.com>
2022-02-05 15:21:57 +1300

    jquery-rails gem removed. Use CDN for jquery - could later add to vendor js folder

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link jquery3.js
 //= link jquery-ui.js
 //= link jquery-ui.css
 //= link gritter.css.scss
