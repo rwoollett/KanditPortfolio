e2f63e2506241179865fc863d829327456c10cf1
Rodney Woollett <rodney.woollett@gmail.com>
2022-02-12 00:08:03 +1300

    manifest line

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,7 @@
 //= link_tree ../fonts
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-// link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link jquery-ui.js
 //= link jquery-ui.css
 //= link gritter.css.scss
